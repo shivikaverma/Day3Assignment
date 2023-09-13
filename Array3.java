1d328a7d6ef3668fd896956c109084c1b77bf746
shivikaverma <107605750+shivikaverma@users.noreply.github.com>
2023-09-13 16:46:03 +0530

    Create Array3.java

---
+++ Array3.java
@@ -0,0 +1,16 @@
+package assignmentthree;
+
+public class Array3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr = {1,2,3,4,5,6,7,8,9,10};
+		 arr[0]=12;
+		 arr[8]=20;
+		
+		for(int i=0; i< arr.length;i++) {
+			System.out.println(arr[i] +" " );
+			
+		}
+    }
+}	
