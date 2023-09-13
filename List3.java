fd0e61b923f6350f4ea95b1f9bc360bbdbdd895b
shivikaverma <107605750+shivikaverma@users.noreply.github.com>
2023-09-13 16:49:05 +0530

    Create List3.java

---
+++ List3.java
@@ -0,0 +1,32 @@
+package assignmentthree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class List3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<Integer> arr = new ArrayList<>();
+		  
+        arr.add(1);
+        arr.add(2);
+        arr.add(3);
+        arr.add(4);
+        arr.add(5);
+        arr.add(6);
+        arr.add(7);
+        
+        System.out.println(arr);
+        
+        arr.remove(0);
+        arr.remove(0);
+        arr.remove(0);
+        arr.remove(0);
+        arr.remove(0);
+        System.out.println(arr);
+ 
+
+	}
+
+}
